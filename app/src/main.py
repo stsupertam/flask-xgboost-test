7c07293b2eee2da88deb25dbaa08301f6d411716
stsupertam <santitham.anan@gmail.com>
2021-09-08 01:25:51 +0700

    add new project structure

--- app/src/main.py
+++ app/src/main.py
@@ -1,14 +1,20 @@
-from flask import Flask, jsonify
+# ref: https://stackoverflow.com/questions/28795561/support-multiple-api-versions-in-flask/28797512
+
+from flask import (
+    Flask,
+    jsonify,
+    request
+)
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def hello_world():
+def index():
     return 'Hello World'
 
 
-@app.route('/predict', methods=['POST'])
-def xgboost_predict():
+@app.route('/v1/questionnaire-food', methods=['POST'])
+def questionnaire_food():
     data = request.json
     return jsonify(data)
 
